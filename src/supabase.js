6ecc930e74204d5df3c0866229277c752c58c7f6
vindeshwariprasad <vinayguptasia53@gmail.com>
2023-11-04 21:08:33 +0530

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://gzujwjkmfbzsojtbdany.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6dWp3amttZmJ6c29qdGJkYW55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg3ODYzMjAsImV4cCI6MjAxNDM2MjMyMH0.K12U7OzLKjQs8NKbGd-_OagxxmFMj8NBD7BJa7DI1Gk";
+  "your key";
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
