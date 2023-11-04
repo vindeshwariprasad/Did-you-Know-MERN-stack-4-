7229e0e727c09a1d2671ed804152024c31c5b146
vindeshwariprasad <vinayguptasia53@gmail.com>
2023-11-04 21:09:44 +0530

    Update script.js

--- okay/script.js
+++ okay/script.js
@@ -59,9 +59,9 @@ async function loadFacts() {
     {
       headers: {
         apikey:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6dWp3amttZmJ6c29qdGJkYW55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg3ODYzMjAsImV4cCI6MjAxNDM2MjMyMH0.K12U7OzLKjQs8NKbGd-_OagxxmFMj8NBD7BJa7DI1Gk",
+          "your key",
         authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6dWp3amttZmJ6c29qdGJkYW55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg3ODYzMjAsImV4cCI6MjAxNDM2MjMyMH0.K12U7OzLKjQs8NKbGd-_OagxxmFMj8NBD7BJa7DI1Gk",
+          "Bearer your key",
       },
     }
   );
